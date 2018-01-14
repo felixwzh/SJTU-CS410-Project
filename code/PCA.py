aae0882ea456e42dbea91076d86667b20a1a5cdf
Wang Zhenghui <1144275914@qq.com>
2018-01-14 17:27:51 +0800

    finish Experiment, and SVM in Results

--- code/PCA.py
+++ code/PCA.py
@@ -8,20 +8,20 @@ n_components=3400
 # n_components = 0.90
 # n_components = 0.85
 
-# n_components=[200,400,1200]
-# for i in range(3):
+n_components=[100,200,400,500,1000,1200,2000,3400]
+for n_component in n_components:
 
-pca = PCA(n_components=n_components)
+    pca = PCA(n_components=n_component)
 
-X = np.load("./../data/all_raw_data.npy")
+    X = np.load("./../data/all_raw_data.npy")
 
-pca.fit(X)
+    pca.fit(X)
 
-newX=pca.transform(X)
+    # newX=pca.transform(X)
 
-np.save("./../data/all_PCA_"+str(pca.n_components_)+".npy",newX)
+    # np.save("./../data/all_PCA_"+str(pca.n_components_)+".npy",newX)
 
-# print pca.explained_variance_ratio_
-# print pca.explained_variance_
-print pca.n_components_, sum(pca.explained_variance_ratio_)
+    # print pca.explained_variance_ratio_
+    # print pca.explained_variance_
+    print pca.n_components_, sum(pca.explained_variance_ratio_)
 
